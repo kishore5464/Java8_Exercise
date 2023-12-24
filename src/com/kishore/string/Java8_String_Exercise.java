9eec9ffe61c4a9d4faea66a8ee0d2dac2b391cca
Kishore Kumar <kishorekumar@Kishores-MacBook-Air.local>
2023-12-25 00:48:27 +0530

    Added more Exercises

--- src/com/kishore/string/Java8_String_Exercise.java
+++ src/com/kishore/string/Java8_String_Exercise.java
@@ -1,12 +1,18 @@
 package com.kishore.string;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Java8_String_Exercise {
     public static void main(String[] args) {
         String name = "Kishore Kumar Arumugam";
         System.out.println("Real String -> " +name);
+
+        String s = "Rotator";
+        String s1 = "kishore";
+        String s2 = "ishoKre";
 
         // 1. One Occurrence Characters
         oneOccurrenceDuplicateAndNonDuplicateCharacters(name);
@@ -22,6 +28,34 @@ public class Java8_String_Exercise {
 
         // 5. Increment and Decrement each Character to its Next Character (e.g. If we have 'A' need to increment to 'B')
         incrementAndDecrementEachCharacterToNextChar(name);
+
+        // 6. Anagram Check for the given 2 Strings
+        anagramStrings(s1, s2);
+
+        // 7. Palindrome Check fot the given String
+        palindromeCheck(s);
+    }
+
+    private static void palindromeCheck(String s) {
+        String temp = s.chars().mapToObj(c -> (char) c).map(Character::toLowerCase).map(String::valueOf).collect(Collectors.joining("", "", ""));
+
+        if(temp.equals(s.toLowerCase())) {
+            System.out.println("The Given String is a Palindrome -> "+temp);
+        } else {
+            System.out.println("The Given String is Not a Palindrome -> "+temp);
+        }
+    }
+
+    private static void anagramStrings(String s1, String s2) {
+        s1 = s1.chars().mapToObj(c-> (char)c).map(Character::toLowerCase).sorted().map(String::valueOf).collect(Collectors.joining());
+        s2 = s2.chars().mapToObj(c-> (char)c).map(Character::toLowerCase).sorted().map(String::valueOf).collect(Collectors.joining());
+
+        if(s1.equals(s2)) {
+            System.out.println("The Given String is an Anagram -> "+s1.equals(s2));
+        }
+        else {
+            System.out.println("The Given String is Not an Anagram -> "+s1.equals(s2));
+        }
     }
 
     private static void incrementAndDecrementEachCharacterToNextChar(String name) {
